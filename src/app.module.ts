77cd4ac0e0f90b5df13f38659b7ddc06f9273b75
kiran gautam <kirangautam225@gmail.com>
2023-01-07 20:55:53 +0545

    operation for lesson

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
+import { LessonModule } from './lesson/lesson.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb+srv://kiranG:kiran123@kirancluster.5tz7c7z.mongodb.net/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+    StudentModule,
+  ],
   controllers: [],
   providers: [],
 })
